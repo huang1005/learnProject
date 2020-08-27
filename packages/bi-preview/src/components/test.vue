56473f05216bf84812d4df271dbd25eeba5656f1
huang1005 <huang201534110@163.com>
2020-08-27 15:15:16 +0800

    创建组件

---
+++ packages/bi-preview/src/components/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>{{values || '暂无数据'}}</div>
+</template>
+
+<script>
+export default {
+    name: '',
+    props: {
+        values: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+
+        };
+    }
+};
+</script>
+
+<style scoped lang='scss' rel='stylesheet/scss'>
+</style>
